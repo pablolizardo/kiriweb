db797a166b458e9e74a92125ed89f8a47bad316f
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2014-09-24 02:26:09 -0300

    late night commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
           navigation : false, // Show next and prev buttons
           slideSpeed : 300,
           paginationSpeed : 400,
-          singleItem:true
+          singleItem:true,
+          itemsScaleUp:true,
+          responsive: true
       });
 
     function parallax () {
